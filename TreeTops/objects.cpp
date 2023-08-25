34a6a16512b23a4ef6fa63352d3e103dc50b8f77
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2023-08-25 19:27:03 +0200

    Added missing .PVM for Unidus C

--- TreeTops/objects.cpp
+++ TreeTops/objects.cpp
@@ -104,8 +104,9 @@ TEX_PVMTABLE TEXTURELIST_TreeTops[] = {
 	{ "TreeTops_Objects", &TEXLIST_TTObjects },
 	{ "TreeTops_Dragons", &TEXLIST_TTDragons },
 	{ "TreeTops_HUD", &TEXLIST_TTHUD },
+	{ "UNI_C_UNIBODY", (TexList*)0x96DC48 },
 	{ "E_SARU", (TexList*)0x962560 },
-	{ "E_BOMB", (TexList*)0x96B464 },
+	{ "E_BOMB", (TexList*)0x96B464 },	
 	{ 0 }
 };
 
